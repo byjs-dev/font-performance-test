fe1285cb1ab7b441297a05d3c75617fcec27da44
Joerg Schneider <joerg.schneider@zeppelin.com>
2023-06-05 11:46:26 +0200

    chore remove CSP header

--- next.config.js
+++ next.config.js
@@ -1,23 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const ContentSecurityPolicy = `
-  connect-src vitals.vercel-insights.com;
-`;
-
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
