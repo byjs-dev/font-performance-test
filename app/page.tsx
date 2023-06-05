9b9f2c01c8484e706acba365594622ed156ba557
Joerg Schneider <joerg.schneider@zeppelin.com>
2023-06-05 13:19:39 +0200

    chore: font and text adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,30 @@ export default function Home() {
   return (
     <div className="flex min-h-screen max-w-3xl flex-col mx-auto gap-2 p-24 font-roboto">
       <header>
-        <h1 className="font-black text-xl">FONT PERFORMANCE TEST</h1>
+        <h1 className="font-black text-xl italic">TITLE - ROBOTO FLEX BLACK</h1>
       </header>
       <main>
-        <h2 className="mt-8 mb-6 text-lg font-bold">Introduction text</h2>
+        <h2 className="mt-8 mb-6 text-lg font-bold">
+          Headline - Roboto Flex bold
+        </h2>
         <p>
-          Labore eu eu culpa est voluptate. Incididunt deserunt commodo id
-          labore velit adipisicing cupidatat nisi est occaecat laborum cillum
-          nostrud. Eiusmod aliquip ut commodo anim eiusmod consectetur consequat
-          voluptate magna. Excepteur dolor fugiat fugiat eu. Aute quis voluptate
-          id do voluptate qui.
+          Roboto Flex regular - Labore eu eu culpa est voluptate. Incididunt
+          deserunt commodo id labore velit adipisicing cupidatat nisi est
+          occaecat laborum cillum nostrud. Eiusmod aliquip ut commodo anim
+          eiusmod consectetur consequat voluptate magna. Excepteur dolor fugiat
+          fugiat eu. Aute quis voluptate id do voluptate qui.
         </p>
         <section className="grid mt-10 gap-6">
-          <h3 className="font-bold text-md">Subheadline</h3>
+          <h3 className="font-bold text-md">Subheadline - Roboto Flex bold</h3>
           <p>
-            Exercitation veniam do elit exercitation voluptate sunt nostrud ea
-            incididunt commodo aliquip tempor sint dolore ut. Consequat aliqua
-            ea cupidatat velit quis eu ea non elit occaecat veniam commodo elit
-            anim. Tempor sunt pariatur excepteur. Elit cillum velit excepteur
-            reprehenderit sint ut duis nostrud incididunt incididunt dolor sunt
-            pariatur eu ad. Quis do nostrud officia qui nulla ad quis enim non
-            ullamco enim id. Irure proident nulla sint laborum sit ea fugiat
-            fugiat.
+            Roboto Flex regular - Exercitation veniam do elit exercitation
+            voluptate sunt nostrud ea incididunt commodo aliquip tempor sint
+            dolore ut. Consequat aliqua ea cupidatat velit quis eu ea non elit
+            occaecat veniam commodo elit anim. Tempor sunt pariatur excepteur.
+            Elit cillum velit excepteur reprehenderit sint ut duis nostrud
+            incididunt incididunt dolor sunt pariatur eu ad. Quis do nostrud
+            officia qui nulla ad quis enim non ullamco enim id. Irure proident
+            nulla sint laborum sit ea fugiat fugiat.
           </p>
           <p>
             Quis in laborum excepteur consequat ipsum mollit adipisicing
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,16 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        roboto: ["var(--font-roboto-flex)"],
+        roboto: [
+          "var(--font-roboto-flex)",
+          "sans-serif",
+          {
+            fontStretch: 108,
+            fontFeatureSettings: '"liga" off',
+            fontVariationSettings:
+              "'GRAD' -200, 'slnt' -10, 'XTRA' 468, 'XOPQ' 96, 'YOPQ' 69, 'YTLC' 514, 'YTUC' 712, 'YTAS' 750, 'YTDE' -203, 'YTFI' 738, 'opsz' 70",
+          },
+        ],
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
